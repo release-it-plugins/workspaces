5e6e97bdade19e1980e8e0796ff896dfd155cb91
蔡二伟 <caierwei@baijia.com>
2023-08-18 18:29:05 +0800

    feat: Adding publicPath option for npm publishConfig

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const DEFAULT_TAG = 'latest';
 const NPM_BASE_URL = 'https://www.npmjs.com';
 const NPM_DEFAULT_REGISTRY = 'https://registry.npmjs.org';
 const DETECT_TRAILING_WHITESPACE = /\s+$/;
+const NPM_PUBLIC_PATH = '/package';
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -401,8 +402,9 @@ export default class WorkspacesPlugin extends Plugin {
   getReleaseUrl(workspaceInfo) {
     const registry = this.getRegistry();
     const baseUrl = registry !== NPM_DEFAULT_REGISTRY ? registry : NPM_BASE_URL;
+    const publicPath = this.getPublicPath() || NPM_PUBLIC_PATH;
 
-    return urlJoin(baseUrl, 'package', workspaceInfo.name);
+    return urlJoin(baseUrl, publicPath, workspaceInfo.name);
   }
 
   getRegistry() {
